3fabcb857654bbd7aab3060351f293680660ef2c
MartinRB45534 <martin.rialhe-badet@etu.univ-savoie.fr>
2022-04-11 17:36:12 +0200

    presque fin de cours 11/04

--- Projet_2D/matrices_elementaires.py
+++ Projet_2D/matrices_elementaires.py
@@ -2,11 +2,11 @@ from numpy import *
 
 from scipy import *
 
-# def matrices_elementaires(P1,P2,P3):
-#     M_masse = array([[1/12,1/24,1/24],[1/24,1/12,1/24],[1/24,1/24,1/12]])
-#     Aire2 = abs((P2[0]-P1[0])*(P3[1]-P1[1])-(P3[0]-P1[0])*(P2[1]-P1[1]))
-#     M_masse*=Aire2
-#     return M_masse
+def matrices_elementaires_masse(P1,P2,P3):
+    M_masse = array([[1/12,1/24,1/24],[1/24,1/12,1/24],[1/24,1/24,1/12]])
+    Aire2 = abs((P2[0]-P1[0])*(P3[1]-P1[1])-(P3[0]-P1[0])*(P2[1]-P1[1]))
+    M_masse*=Aire2
+    return M_masse
 
 def matrices_elementaires(P1,P2,P3):
     C=array([[dot(P3-P1,P3-P1),-dot(P2-P1,P3-P1)],[-dot(P2-P1,P3-P1),dot(P2-P1,P2-P1)]])
